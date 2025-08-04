daa2a4bbe273ef95d5bada5f4d641b81f92e3019
NicolasMollerke <nicolasmollerke@gmail.com>
2025-08-04 18:13:08 -0300

    Add files via upload

---
+++ Pokemon/pokemon.pl
@@ -0,0 +1,315 @@
+pokemon(victini).
+pokemon(snivy).
+pokemon(servine).
+pokemon(serperior).
+pokemon(tepig).
+pokemon(pignite).
+pokemon(emboar).
+pokemon(oshawott).
+pokemon(dewott).
+pokemon(samurott).
+pokemon(patrat).
+pokemon(watchog).
+pokemon(lillipup).
+pokemon(herdier).
+pokemon(stoutland).
+pokemon(purrloin).
+pokemon(liepard).
+pokemon(pansage).
+pokemon(simisage).
+pokemon(pansear).
+pokemon(simisear).
+pokemon(panpour).
+pokemon(simipour).
+pokemon(munna).
+pokemon(musharna).
+pokemon(pidove).
+pokemon(tranquill).
+pokemon(unfezant).
+pokemon(blitzle).
+pokemon(zebstrika).
+pokemon(roggenrola).
+pokemon(boldore).
+pokemon(gigalith).
+pokemon(woobat).
+pokemon(swoobat).
+pokemon(drilbur).
+pokemon(excadrill).
+pokemon(audino).
+pokemon(timburr).
+pokemon(gurdurr).
+pokemon(conkeldurr).
+pokemon(tympole).
+pokemon(palpitoad).
+pokemon(seismitoad).
+pokemon(throh).
+pokemon(sawk).
+pokemon(sewaddle).
+pokemon(swadloon).
+pokemon(leavanny).
+pokemon(venipede).
+pokemon(whirlipede).
+pokemon(scolipede).
+pokemon(cottonee).
+pokemon(whimsicott).
+pokemon(petilil).
+pokemon(lilligant).
+pokemon(basculin).
+pokemon(sandile).
+pokemon(krokorok).
+pokemon(krookodile).
+pokemon(darumaka).
+pokemon(darmanitan).
+pokemon(maractus).
+pokemon(dwebble).
+pokemon(crustle).
+pokemon(scraggy).
+pokemon(scrafty).
+pokemon(sigilyph).
+pokemon(yamask).
+pokemon(cofagrigus).
+pokemon(tirtouga).
+pokemon(carracosta).
+pokemon(archen).
+pokemon(archeops).
+pokemon(trubbish).
+pokemon(garbodor).
+pokemon(zorua).
+pokemon(zoroark).
+pokemon(minccino).
+pokemon(cinccino).
+pokemon(gothita).
+pokemon(gothorita).
+pokemon(gothitelle).
+pokemon(solosis).
+pokemon(duosion).
+pokemon(reuniclus).
+pokemon(ducklett).
+pokemon(swanna).
+pokemon(vanillite).
+pokemon(vanillish).
+pokemon(vanilluxe).
+pokemon(deerling).
+pokemon(sawsbuck).
+pokemon(emolga).
+pokemon(karrablast).
+pokemon(escavalier).
+pokemon(foongus).
+pokemon(amoonguss).
+pokemon(frillish).
+pokemon(jellicent).
+pokemon(alomomola).
+pokemon(joltik).
+pokemon(galvantula).
+pokemon(ferroseed).
+pokemon(ferrothorn).
+pokemon(klink).
+pokemon(klang).
+pokemon(klinklang).
+pokemon(tynamo).
+pokemon(eelektrik).
+pokemon(eelektross).
+pokemon(elgyem).
+pokemon(beheeyem).
+pokemon(litwick).
+pokemon(lampent).
+pokemon(chandelure).
+pokemon(axew).
+pokemon(fraxure).
+pokemon(haxorus).
+pokemon(cubchoo).
+pokemon(beartic).
+pokemon(cryogonal).
+pokemon(shelmet).
+pokemon(accelgor).
+pokemon(stunfisk).
+pokemon(mienfoo).
+pokemon(mienshao).
+pokemon(druddigon).
+pokemon(golett).
+pokemon(golurk).
+pokemon(pawniard).
+pokemon(bisharp).
+pokemon(bouffalant).
+pokemon(rufflet).
+pokemon(braviary).
+pokemon(vullaby).
+pokemon(mandibuzz).
+pokemon(heatmor).
+pokemon(durant).
+pokemon(deino).
+pokemon(zweilous).
+pokemon(hydreigon).
+pokemon(larvesta).
+pokemon(volcarona).
+pokemon(cobalion).
+pokemon(terrakion).
+pokemon(virizion).
+pokemon(tornadus).
+pokemon(thundurus).
+pokemon(reshiram).
+pokemon(zekrom).
+pokemon(landorus).
+pokemon(kyurem).
+pokemon(keldeo).
+pokemon(meloetta).
+pokemon(genesect).
+
+tipo(victini, fogo).
+tipo(snivy, grama).
+tipo(servine, grama).
+tipo(serperior, grama).
+tipo(tepig, fogo).
+tipo(pignite, fogo).
+tipo(emboar, fogo).
+tipo(oshawott, agua).
+tipo(dewott, agua).
+tipo(samurott, agua).
+tipo(patrat, normal).
+tipo(watchog, normal).
+tipo(lillipup, normal).
+tipo(herdier, normal).
+tipo(stoutland, normal).
+tipo(purrloin, sombrio).
+tipo(liepard, sombrio).
+tipo(pansage, grama).
+tipo(simisage, grama).
+tipo(pansear, fogo).
+tipo(simisear, fogo).
+tipo(panpour, agua).
+tipo(simipour, agua).
+tipo(munna, psiquico).
+tipo(musharna, psiquico).
+tipo(pidove, normal).
+tipo(tranquill, normal).
+tipo(unfezant, normal).
+tipo(blitzle, eletrico).
+tipo(zebstrika, eletrico).
+tipo(roggenrola, pedra).
+tipo(boldore, pedra).
+tipo(gigalith, pedra).
+tipo(woobat, psiquico).
+tipo(swoobat, psiquico).
+tipo(drilbur, terra).
+tipo(excadrill, terra).
+tipo(audino, normal).
+tipo(timburr, lutador).
+tipo(gurdurr, lutador).
+tipo(conkeldurr, lutador).
+tipo(tympole, agua).
+tipo(palpitoad, agua).
+tipo(seismitoad, agua).
+tipo(throh, lutador).
+tipo(sawk, lutador).
+tipo(sewaddle, inseto).
+tipo(swadloon, inseto).
+tipo(leavanny, inseto).
+tipo(venipede, inseto).
+tipo(whirlipede, inseto).
+tipo(scolipede, inseto).
+tipo(cottonee, grama).
+tipo(whimsicott, grama).
+tipo(petilil, grama).
+tipo(lilligant, grama).
+tipo(basculin, agua).
+tipo(sandile, terra).
+tipo(krokorok, terra).
+tipo(krookodile, terra).
+tipo(darumaka, fogo).
+tipo(darmanitan, fogo).
+tipo(maractus, grama).
+tipo(dwebble, inseto).
+tipo(crustle, inseto).
+tipo(scraggy, sombrio).
+tipo(scrafty, sombrio).
+tipo(sigilyph, psiquico).
+tipo(yamask, fantasma).
+tipo(cofagrigus, fantasma).
+tipo(tirtouga, agua).
+tipo(carracosta, agua).
+tipo(archen, pedra).
+tipo(archeops, pedra).
+tipo(trubbish, venenoso).
+tipo(garbodor, venenoso).
+tipo(zorua, sombrio).
+tipo(zoroark, sombrio).
+tipo(minccino, normal).
+tipo(cinccino, normal).
+tipo(gothita, psiquico).
+tipo(gothorita, psiquico).
+tipo(gothitelle, psiquico).
+tipo(solosis, psiquico).
+tipo(duosion, psiquico).
+tipo(reuniclus, psiquico).
+tipo(ducklett, agua).
+tipo(swanna, agua).
+tipo(vanillite, gelo).
+tipo(vanillish, gelo).
+tipo(vanilluxe, gelo).
+tipo(deerling, normal).
+tipo(sawsbuck, normal).
+tipo(emolga, eletrico).
+tipo(karrablast, inseto).
+tipo(escavalier, inseto).
+tipo(foongus, grama).
+tipo(amoonguss, grama).
+tipo(frillish, agua).
+tipo(jellicent, agua).
+tipo(alomomola, agua).
+tipo(joltik, inseto).
+tipo(galvantula, inseto).
+tipo(ferroseed, grama).
+tipo(ferrothorn, grama).
+tipo(klink, metal).
+tipo(klang, metal).
+tipo(klinklang, metal).
+tipo(tynamo, eletrico).
+tipo(eelektrik, eletrico).
+tipo(eelektross, eletrico).
+tipo(elgyem, psiquico).
+tipo(beheeyem, psiquico).
+tipo(litwick, fantasma).
+tipo(lampent, fantasma).
+tipo(chandelure, fantasma).
+tipo(axew, dragao).
+tipo(fraxure, dragao).
+tipo(haxorus, dragao).
+tipo(cubchoo, gelo).
+tipo(beartic, gelo).
+tipo(cryogonal, gelo).
+tipo(shelmet, inseto).
+tipo(accelgor, inseto).
+tipo(stunfisk, terra).
+tipo(mienfoo, lutador).
+tipo(mienshao, lutador).
+tipo(druddigon, dragao).
+tipo(golett, terra).
+tipo(golurk, terra).
+tipo(pawniard, sombrio).
+tipo(bisharp, sombrio).
+tipo(bouffalant, normal).
+tipo(rufflet, normal).
+tipo(braviary, normal).
+tipo(vullaby, sombrio).
+tipo(mandibuzz, sombrio).
+tipo(heatmor, fogo).
+tipo(durant, inseto).
+tipo(deino, sombrio).
+tipo(zweilous, sombrio).
+tipo(hydreigon, sombrio).
+tipo(larvesta, inseto).
+tipo(volcarona, inseto).
+tipo(cobalion, metal).
+tipo(terrakion, pedra).
+tipo(virizion, grama).
+tipo(tornadus, voador).
+tipo(thundurus, eletrico).
+tipo(reshiram, dragao).
+tipo(zekrom, dragao).
+tipo(landorus, terra).
+tipo(kyurem, dragao).
+tipo(keldeo, agua).
+tipo(meloetta, normal).
+tipo(genesect, inseto).
+
+TiposUnicos = [agua, dragao, eletrico, fantasma, fogo, gelo, grama, inseto, lutador, metal, normal, pedra, psiquico, sombrio, terra, venenoso, voador].
